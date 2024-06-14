82307a02dfe6ef80ffa4bf70e747faaf9547e889
Joe Moore <joe.moore@gmail.com>
2024-06-14 12:09:31 -0600

    <enrichment-processor-start> Begin enrichment processor lab

---
+++ cashcard-transaction-enricher/src/main/java/example/cashcard/domain/Transaction.java
@@ -0,0 +1,4 @@
+package example.cashcard.domain;
+
+public record Transaction(Long id, CashCard cashCard) {
+}
---
+++ cashcard-transaction-enricher/src/main/java/example/cashcard/service/EnrichmentService.java
@@ -0,0 +1,4 @@
+package example.cashcard.service;
+
+public class EnrichmentService {
+}

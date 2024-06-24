577194ba40ae2c82ef92ef292132cbf075a6d3bc
Joe Moore <joe.moore@gmail.com>
2024-06-24 15:29:17 -0600

    <sink-end> End sink lab

--- cashcard-transaction-sink/src/main/java/example/cashcard/sink/CashCardTransactionSink.java
+++ cashcard-transaction-sink/src/main/java/example/cashcard/sink/CashCardTransactionSink.java
@@ -1,8 +1,60 @@
 package example.cashcard.sink;
 
+import example.cashcard.domain.EnrichedTransaction;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.StringJoiner;
+import java.util.function.Consumer;
 
 @Configuration
 public class CashCardTransactionSink {
 
+  public static final String CSV_FILE_PATH = System.getProperty("user.dir") + "/build/tmp/transactions-output.csv";
+
+  @Bean
+  public Consumer<EnrichedTransaction> sinkToConsole() {
+    return enrichedTransaction -> {
+      System.out.println("Transaction Received: " + enrichedTransaction);
+    };
+  }
+
+  @Bean
+  public Consumer<EnrichedTransaction> cashCardTransactionFileSink() {
+    return enrichedTransaction -> {
+      StringJoiner joiner = new StringJoiner(",");
+      StringJoiner enrichedTxnTextual = joiner.add(String.valueOf(enrichedTransaction.id()))
+        .add(String.valueOf(enrichedTransaction.cashCard().id()))
+        .add(String.valueOf(enrichedTransaction.cashCard().amountRequestedForAuth()))
+        .add(enrichedTransaction.cardHolderData().name())
+        .add(enrichedTransaction.cardHolderData().userId().toString())
+        .add(enrichedTransaction.cardHolderData().address())
+        .add(enrichedTransaction.approvalStatus().name());
+
+      Path path = Paths.get(CSV_FILE_PATH);
+
+      try {
+        ensureOutputFileExists();
+        Files.writeString(path, enrichedTxnTextual.toString() + "\n", StandardOpenOption.APPEND);
+      }
+      catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+
+    };
+  }
+
+  private void ensureOutputFileExists() {
+    try {
+      new File(CSV_FILE_PATH).createNewFile();
+    } catch (IOException e) {
+      // Ignore
+    };
+  }
 }
--- cashcard-transaction-sink/src/test/java/example/cashcard/sink/CashCardTransactionSinkTests.java
+++ cashcard-transaction-sink/src/test/java/example/cashcard/sink/CashCardTransactionSinkTests.java
@@ -1,18 +1,70 @@
 package example.cashcard.sink;
 
+import example.cashcard.domain.*;
+import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+import org.springframework.cloud.stream.binder.test.InputDestination;
+import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
+import org.springframework.context.annotation.Import;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.Message;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.util.List;
+import java.util.UUID;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(properties = "spring.cloud.function.definition=sinkToConsole;cashCardTransactionFileSink")
+@Import(TestChannelBinderConfiguration.class)
+@ExtendWith(OutputCaptureExtension.class)
 public class CashCardTransactionSinkTests {
+  private static final int AWAIT_DURATION = 10;
 
   @Test
-  void cashCardSinkToConsole() throws IOException {
+  void sinkToConsole(@Autowired InputDestination inputDestination, CapturedOutput output) throws IOException {
+    Transaction transaction = new Transaction(1L, new CashCard(123L, "John Doe", 1.00));
+    EnrichedTransaction enrichedTransaction = new EnrichedTransaction(transaction.id(), transaction.cashCard(), ApprovalStatus.APPROVED,
+      new CardHolderData(UUID.randomUUID(), transaction.cashCard().owner(), "123 Main Street"));
 
+    Message<EnrichedTransaction> message = MessageBuilder.withPayload(enrichedTransaction).build();
+
+    inputDestination.send(message, "sinkToConsole-in-0");
+
+    Awaitility.await().atMost(Duration.ofSeconds(AWAIT_DURATION))
+      .until(() -> output.toString().contains("Transaction Received: " + enrichedTransaction.toString()));
   }
+
+  @Test
+  void sinkToFile(@Autowired InputDestination inputDestination) throws IOException {
+    Transaction transaction = new Transaction(1L, new CashCard(123L, "John Doe", 100.25));
+    UUID uuid = UUID.fromString("65d0b699-3695-44c6-ba23-4a241717dab7");
+    EnrichedTransaction enrichedTransaction = new EnrichedTransaction(transaction.id(), transaction.cashCard(), ApprovalStatus.APPROVED,
+      new CardHolderData(uuid, transaction.cashCard().owner(), "123 Main Street"));
+
+    Message<EnrichedTransaction> message = MessageBuilder.withPayload(enrichedTransaction).build();
+
+    inputDestination.send(message, "cashCardTransactionFileSink-in-0");
+
+    Path path = Paths.get(CashCardTransactionSink.CSV_FILE_PATH);
+    Awaitility.await().until(() -> Files.exists(path));
+
+    List<String> lines = Files.readAllLines(path);
+
+    assertThat(lines.get(0)).isEqualTo
+      ("1,123,100.25,John Doe,65d0b699-3695-44c6-ba23-4a241717dab7,123 Main Street,APPROVED");
+  }
+
 
   @SpringBootApplication
   public static class App {

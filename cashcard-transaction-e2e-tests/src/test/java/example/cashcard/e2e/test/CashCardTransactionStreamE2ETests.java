6ccb5ef9a930aa007f7853ab81a1d5f9012eb564
Joe Moore <joe.moore@gmail.com>
2024-07-01 14:50:23 -0600

    <e2e-tests-start> start of End-to-End Tests lab

---
+++ cashcard-transaction-e2e-tests/src/test/java/example/cashcard/e2e/test/CashCardTransactionStreamE2ETests.java
@@ -0,0 +1,14 @@
+package example.cashcard.e2e.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+
+@SpringBootTest
+public class CashCardTransactionStreamE2ETests {
+
+  @Test
+  void cashCardTransactionStreamEndToEnd() throws IOException {
+  }
+}
